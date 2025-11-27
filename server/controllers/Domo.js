40ca2109816b3ebabbb176977a93837cbe1cfcbe
Mariana Espinal <mce1571@g.rit.edu>
2025-11-27 12:28:44 -0500

    client hosted and server

---
+++ server/controllers/Domo.js
@@ -0,0 +1,50 @@
+const models = require('../models');
+
+const { Domo } = models;
+
+const makerPage = async (req, res) => res.render('app');
+
+const getDomos = async (req, res) => {
+  try {
+    const query = { owner: req.session.account._id };
+    const docs = await Domo.find(query).select('name age occupation').lean().exec();
+    return res.json({ domos: docs });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: 'Error retrieving domos!' });
+  }
+};
+
+const makeDomo = async (req, res) => {
+  if (!req.body.name || !req.body.age || !req.body.occupation) {
+    return res.status(400).json({ error: 'Both name age  and occupation are required!' });
+  }
+
+  const domoData = {
+    name: req.body.name,
+    age: req.body.age,
+    occupation: req.body.occupation,
+    owner: req.session.account._id,
+  };
+  try {
+    const newDomo = new Domo(domoData);
+    await newDomo.save();
+    return res.status(201).json({
+      name: newDomo.name,
+      age: newDomo.age,
+      occupation: newDomo.occupation,
+    });
+  } catch (err) {
+    console.log(err);
+    if (err.code === 11000) {
+      return res.status(400).json({ error: 'Domo already exists!' });
+    }
+    return res.status(500).json({ error: 'an error occured making domo!' });
+  }
+};
+
+module.exports = {
+  makerPage,
+  makeDomo,
+  getDomos,
+};
---
+++ server/models/Domo.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const _ = require('underscore');
+
+const setName = (name) => _.escape(name).trim();
+const setJob = (job) => _.escape(job).trim();
+
+const DomoSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    set: setName,
+  },
+  age: {
+    type: Number,
+    min: 0,
+    required: true,
+  },
+   occupation: {
+    type: String,
+    required: true,
+    trim: true,
+    set: setJob,
+  },
+  owner: {
+    type: mongoose.Schema.ObjectId,
+    required: true,
+    ref: 'Account',
+  },
+  createdDate: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+DomoSchema.statics.toAPI = (doc) => ({
+  name: doc.name,
+  age: doc.age,
+  occupation: doc.occupation,
+});
+
+const DomoModel = mongoose.model('Domo', DomoSchema);
+
+module.exports = DomoModel;
